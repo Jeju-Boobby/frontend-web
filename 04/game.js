a9a040501312641d3a73dd185a961f071f3ff2b1
Jeju-Boobby <boo1984@naver.com>
2017-10-09 15:29:49 +0900

    css 수정 및 네모 랜덤 위치 설정

--- 04/game.js
+++ 04/game.js
@@ -19,17 +19,28 @@ function updateScoreBoard() {
   max_missDom.innerHTML = max_miss;
 }
 
+console.log(document.body.scrollWidth);
+console.log(document.body.scrollHeight);
+var rectangleDom;
+
 function showRectangle() {
+  var left = document.body.scrollWidth - 30; // 30은 rectangle 높이
+  var top = document.body.scrollHeight - 145; // 129는 header, hr, rectangle 높이를 합한 값
   var str = '<span id="rectangle"></span>';
   mainDom.innerHTML = str;
+
+  var leftRandom = Math.floor(Math.random() * left);
+  var topRandom = Math.floor(Math.random() * top) + 115;
+  console.log(leftRandom);
+  console.log(topRandom);
+  rectangleDom = document.getElementById('rectangle');
+  console.log(rectangleDom);
+  rectangleDom.style.left = leftRandom + "px";
+  rectangleDom.style.top = topRandom + "px";
 }
 
 updateScoreBoard();
 showRectangle();
-
-// main의 width, height 불러오기
-
-// rectangle 위치 랜덤으로 정하기
 
 // rectangle이 클릭됬을 때 실행할 함수
 
